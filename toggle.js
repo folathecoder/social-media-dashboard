565129ac86c69e3f6894bbd215c92db9c9c17da6
folathecoder <officialakinloyefolarin@gmail.com>
2021-05-19 15:17:43 +0100

    Header ==== Added Border Bottom To Title

--- toggle.js
+++ toggle.js
@@ -1,14 +1,14 @@
 'use script';
 
 //TODO: Target all required elements in the DOM
-
 const toggleBtn = document.querySelector('.toggle__btn');
 const toggleBtnInner = document.querySelector('.toggle__btn-inner');
 const themeMode = document.querySelector('.theme-mode');
 const body = document.body;
 const toolBarColor = document.querySelector('.toolBarColor');
 
-toggleBtn.addEventListener('click', e => {
+//TODO: Add a click event listener to "toggleBtn"
+toggleBtn.addEventListener('click', () => {
 
     toggleBtnInner.classList.toggle('toggle__active');
 
@@ -17,14 +17,11 @@ toggleBtn.addEventListener('click', e => {
         body.classList.replace('dark-theme', 'light-theme');
         body.classList.add('theme-effect');
         toolBarColor.content = '#ffffff';
-        console.log(toolBarColor.content);
-
-
     }
+
     else {
         themeMode.innerHTML = '<p>Dark Mode</p>';
         body.classList.replace('light-theme', 'dark-theme');
         toolBarColor.content = '#1e202a';
-        console.log(toolBarColor.content);
     }
-})  
+});  
