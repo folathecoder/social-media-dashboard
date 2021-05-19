493c42327f2a4c4d64c7dc9fe3c11cc0c1cb546b
folathecoder <officialakinloyefolarin@gmail.com>
2021-05-19 11:48:48 +0100

    Tool Bard Color ==== Color Change On Toggle

--- toggle.js
+++ toggle.js
@@ -6,6 +6,7 @@ const toggleBtn = document.querySelector('.toggle__btn');
 const toggleBtnInner = document.querySelector('.toggle__btn-inner');
 const themeMode = document.querySelector('.theme-mode');
 const body = document.body;
+const toolBarColor = document.querySelector('.toolBarColor');
 
 toggleBtn.addEventListener('click', e => {
 
@@ -15,10 +16,15 @@ toggleBtn.addEventListener('click', e => {
         themeMode.innerHTML = '<p>Light Mode</p>';
         body.classList.replace('dark-theme', 'light-theme');
         body.classList.add('theme-effect');
+        toolBarColor.content = '#ffffff';
+        console.log(toolBarColor.content);
+
 
     }
     else {
         themeMode.innerHTML = '<p>Dark Mode</p>';
         body.classList.replace('light-theme', 'dark-theme');
+        toolBarColor.content = '#1e202a';
+        console.log(toolBarColor.content);
     }
 })  
